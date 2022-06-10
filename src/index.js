88c42d29d81e0b581a4aa6b0a765b6c9e056697a
Fabricio Leonardo Sodano Pascazi <fabricio.sodano@gmail.com>
2022-06-10 19:41:33 +1000

    Agrega ejemplo base para construir una aplicación de Stellar

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import { Asset, Keypair, Operation, Server, TransactionBuilder } from "stellar-sdk";
+
+const server = new Server("https://horizon-testnet.stellar.org");
+
+const fee = await server.fetchBaseFee();
+
+const tx = new TransactionBuilder(await server.loadAccount("GC2FYDXGZKVYJXTJ7U2EK4YI6YKVCSHFZQHL24VM5K7V2S3F62FKAJPS"), {
+    fee: fee.toString(),
+    networkPassphrase: 'Test SDF Network ; September 2015'
+}).addOperation(Operation.payment({
+    amount: "1",
+    asset: Asset.native(),
+    destination: "GC2FYDXGZKVYJXTJ7U2EK4YI6YKVCSHFZQHL24VM5K7V2S3F62FKAJPS",
+}))
+    .setTimeout(30).build();
+
+const keyPair = Keypair.fromSecret("SBFOJVJHCKYKICNRCM5E65LTL7AAFJEERSRV6ZC3MMZUOCW2JWEFAA7E");
+tx.sign(keyPair);
+
+try {
+    const response = await server.submitTransaction(tx);
+    console.log(response.id);
+} catch (e) {
+    console.error(e);
+}
